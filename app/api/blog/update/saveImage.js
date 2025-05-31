38bd3d6f5e279c3e0284e4683ad0e8a3466665f5
balaji-nirmit <nirmitpratapsingh11@gmail.com>
2025-05-31 21:48:41 +0530

    IMAGE UPLOAD

--- app/api/blog/update/saveImage.js
+++ app/api/blog/update/saveImage.js
@@ -1,18 +1,18 @@
-'use client'
-import { upload } from '@vercel/blob/client'
-import { v4 as uuidv4 } from 'uuid';
-import { writeFile } from 'fs/promises';
-export default function SaveImage() {
-    const saveImage = async (file) => {
-        if (!file || typeof file === 'string') return null;
-        const ext = path.extname(file.name);
-        const filename = `${uuidv4()}${ext}`;
-        const buffer = Buffer.from(await file.arrayBuffer());
+// 'use client'
+// import { upload } from '@vercel/blob/client'
+// import { v4 as uuidv4 } from 'uuid';
+// import { writeFile } from 'fs/promises';
+// export default function SaveImage() {
+//     const saveImage = async (file) => {
+//         if (!file || typeof file === 'string') return null;
+//         const ext = path.extname(file.name);
+//         const filename = `${uuidv4()}${ext}`;
+//         const buffer = Buffer.from(await file.arrayBuffer());
 
-        blobUrl=await upload(`jozpen/uploads/{filename}`,buffer,{
-            access: 'public',
-            handleUploadUrl:'/api/handleImageUpload',
-        })
-        return blobUrl;
-    };
-}
+//         blobUrl=await upload(`jozpen/uploads/{filename}`,buffer,{
+//             access: 'public',
+//             handleUploadUrl:'/api/handleImageUpload',
+//         })
+//         return blobUrl;
+//     };
+// }
--- app/api/handleImageUpload/route.js
+++ app/api/handleImageUpload/route.js
@@ -1,49 +1,49 @@
-import { auth } from '@/auth';
-import { handleUpload } from '@vercel/blob/client';
-import { NextResponse } from 'next/server';
+// import { auth } from '@/auth';
+// import { handleUpload } from '@vercel/blob/client';
+// import { NextResponse } from 'next/server';
 
-export async function POST(request) {
-    const session = await auth();
-    // Make sure you first check if the user is authenticated!
-    const body = (await request.json());
+// export async function POST(request) {
+//     const session = await auth();
+//     // Make sure you first check if the user is authenticated!
+//     const body = (await request.json());
 
-    try {
-        const jsonResponse = await handleUpload({
-            body,
-            request,
-            onBeforeGenerateToken: async (
-            ) => {
-                return {
-                    allowedContentTypes: ['image/jpeg', 'image/png', 'image/gif'],
-                    allowOverwrite: true,
-                    tokenPayload: JSON.stringify({
-                        userId: session.id,
-                        // optional, sent to your server on upload completion
-                        // you could pass a user id from auth, or a value from clientPayload
-                    }),
-                };
-            },
-            onUploadCompleted: async ({ blob, tokenPayload }) => {
-                // Get notified of client upload completion
-                // ⚠️ This will not work on `localhost` websites,
-                // Use ngrok or similar to get the full upload flow
+//     try {
+//         const jsonResponse = await handleUpload({
+//             body,
+//             request,
+//             onBeforeGenerateToken: async (
+//             ) => {
+//                 return {
+//                     allowedContentTypes: ['image/jpeg', 'image/png', 'image/gif'],
+//                     allowOverwrite: true,
+//                     tokenPayload: JSON.stringify({
+//                         userId: session.id,
+//                         // optional, sent to your server on upload completion
+//                         // you could pass a user id from auth, or a value from clientPayload
+//                     }),
+//                 };
+//             },
+//             onUploadCompleted: async ({ blob, tokenPayload }) => {
+//                 // Get notified of client upload completion
+//                 // ⚠️ This will not work on `localhost` websites,
+//                 // Use ngrok or similar to get the full upload flow
 
-                console.log('blob upload completed', blob, tokenPayload);
+//                 console.log('blob upload completed', blob, tokenPayload);
 
-                try {
-                    // Run any logic after the file upload completed
-                    const { userId, projectId } = JSON.parse(tokenPayload);
-                    console.log(`File uploaded for user: ${userId} and projectId: ${projectId}`)
-                } catch (error) {
-                    console.log(error);
-                    throw new Error('Could not update user');
-                }
-            },
-        });
+//                 try {
+//                     // Run any logic after the file upload completed
+//                     const { userId, projectId } = JSON.parse(tokenPayload);
+//                     console.log(`File uploaded for user: ${userId} and projectId: ${projectId}`)
+//                 } catch (error) {
+//                     console.log(error);
+//                     throw new Error('Could not update user');
+//                 }
+//             },
+//         });
 
-        return NextResponse.json(jsonResponse);
-    } catch (error) {
-        return NextResponse.json({ error: (error).message }, { status: 400 }, // The webhook will retry 5 times waiting for a 200
-        );
-    }
-}
+//         return NextResponse.json(jsonResponse);
+//     } catch (error) {
+//         return NextResponse.json({ error: (error).message }, { status: 400 }, // The webhook will retry 5 times waiting for a 200
+//         );
+//     }
+// }
